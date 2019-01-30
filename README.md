be73b5a17cc85dd24cadeeb86a56f698ba9e67b5
Samuel Vanderwaal <samuel.vanderwaal@gmail.com>
2019-01-29 23:30:16 -0900

    Updates tool to use Factoshi's API and no longer require grant pool numbers as inputs.

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # Factom Grant Pool Future Size Calculator
 
-This is a simple utility tool for calculating the size of the Factom community grant pool at some date in the future. To use you need to get the size of the grant pool currently from [Factoshi.io](https://factoshi.io/price_and_supply) and the daily amount contributed to the pool from the [TFA Explorer](https://explorer.factoid.org/) or [Luciap's tool](https://luciap.ca/#/authority-set). 
+This is a simple utility tool for calculating the size of the Factom community grant pool at future dates. 
 
-Pass these values as arguments to the tool in addition to the ending date you want to calculate the size of the grant pool at. The tool accepts most date and time formats but assumes UTC timezone. 
+## Usage
+To use it, simply pass as a command line argument the desired date at which to calculate the grant pool size. The tool accepts most date and time formats but assumes UTC timezone. 
 
-E.g. `python grant-pool-calculator.py 96412.37 1396.41 "2018-11-14 00:00"` or
-`python grant-pool-calculator.py 104302.91 1401.27 2018-11-17T12:00`.
+E.g. `python grant-pool-calculator.py "2019-03-01 00:00"` or
+`python grant-pool-calculator.py 2019-02-27T12:00`.
+
+## Limitations
+Forecasting into the future past an unpaid grant round will yield inaccurate results. The calculation should be rerun if any ANOs change their efficiencies or there are additions or subtractions to the authority set.
--- grant-pool-calculator.py
+++ grant-pool-calculator.py
@@ -1,4 +1,6 @@
 import sys
+import requests
+import json
 import pytz
 from dateutil import parser, tz, utils
 from datetime import datetime
@@ -6,19 +8,49 @@ from datetime import datetime
 # Create default UTC tzone object
 default_tz = pytz.utc
 
-current_grant_fcts = float(sys.argv[1])
-daily_fct_to_grant_pool = float(sys.argv[2])
+# GraphQL Queries
+grant_pool = """
+{
+	factoids 
+	{    
+	grantPool
+	}
+}
+"""
 
-# Parse end date supplied by user and assume UTC if no timzeone is provided
-end_date = utils.default_tzinfo(parser.parse(sys.argv[3]), default_tz)
+history = """
+{
+	history(name: "grantPool") 
+	{
+    	name
+    	data
+  	}
+}
+ """
 
-current_time = datetime.now(pytz.utc)
+r1 = requests.post("http://159.89.231.170/", json={'query': grant_pool})
+r2 = requests.post("http://159.89.231.170/", json={'query': history})
 
-# Factoshi updates the grant pool size once a day at 00:00 UTC, so create today's date to start then
-today_date = datetime(current_time.year, current_time.month, current_time.day, tzinfo=pytz.utc)
+resp1 = json.loads(r1.text)
+resp2 = json.loads(r2.text)
 
-date_diff = end_date - today_date
-days = float(date_diff.days) + float(date_diff.seconds)/86400
+current_grant_fcts = resp1["data"]["factoids"]["grantPool"]
+
+# All historical grant pool data sorted with most recent first.
+# Data is formatted as lists: [unix_ts, grant_pool_fcts]
+data = resp2["data"]["history"]["data"]
+
+# The first item will almost always be an incomplete day so use the next two
+# for calculating the most recent daily amount going into the grant pool.
+daily_fct_to_grant_pool = float(data[1][1]) - float(data[2][1])
+
+print(daily_fct_to_grant_pool)
+
+# Parse end date supplied by user and assume UTC
+end_time = utils.default_tzinfo(parser.parse(sys.argv[1]), default_tz).timestamp()
+current_time = datetime.now(pytz.utc).timestamp()
+
+days =(end_time - current_time)/86400.0
 future_fct_amount = current_grant_fcts + daily_fct_to_grant_pool * days
 
 print(future_fct_amount)

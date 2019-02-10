e405eac338179c2943a2051d4a6cd25c09c28171
Samuel Vanderwaal <samuel.vanderwaal@gmail.com>
2019-02-10 12:12:18 -0900

    Implements updates to stay in sync with the upstream API.

--- grant-pool-calculator.py
+++ grant-pool-calculator.py
@@ -2,7 +2,7 @@ import sys
 import requests
 import json
 import pytz
-from dateutil import parser, tz, utils
+from dateutil import parser, utils
 from datetime import datetime
 
 # Create default UTC tzone object
@@ -11,20 +11,22 @@ default_tz = pytz.utc
 # GraphQL Queries
 grant_pool = """
 {
-	factoids 
-	{    
-	grantPool
-	}
+    factoids
+    {
+    grantPool
+    }
 }
 """
 
 history = """
 {
-	history(name: "grantPool") 
-	{
-    	name
-    	data
-  	}
+    history
+    {
+        factoids
+            {
+            grantPool
+        }
+    }
 }
  """
 
@@ -36,21 +38,23 @@ resp2 = json.loads(r2.text)
 
 current_grant_fcts = resp1["data"]["factoids"]["grantPool"]
 
-# All historical grant pool data sorted with most recent first.
-# Data is formatted as lists: [unix_ts, grant_pool_fcts]
-data = resp2["data"]["history"]["data"]
+# All historical grant pool data sorted with most recent last.
+# Data is formatted as lists: [unix_ts_ms, grant_pool_fcts]
+data = resp2["data"]["history"]["factoids"]["grantPool"]
 
 # The first item will almost always be an incomplete day so use the next two
 # for calculating the most recent daily amount going into the grant pool.
-daily_fct_to_grant_pool = float(data[1][1]) - float(data[2][1])
+# We start at the end of the list to get the most recent values.
+daily_fct_to_grant_pool = float(data[-2][1]) - float(data[-3][1])
 
 print(daily_fct_to_grant_pool)
 
 # Parse end date supplied by user and assume UTC
-end_time = utils.default_tzinfo(parser.parse(sys.argv[1]), default_tz).timestamp()
+end_time = utils.default_tzinfo(parser.parse(sys.argv[1]),
+                                default_tz).timestamp()
 current_time = datetime.now(pytz.utc).timestamp()
 
-days =(end_time - current_time)/86400.0
+days = (end_time - current_time) / 86400.0
 future_fct_amount = current_grant_fcts + daily_fct_to_grant_pool * days
 
 print(future_fct_amount)
